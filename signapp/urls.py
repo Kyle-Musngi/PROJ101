0bb3de1a8c11cce2cb7b35cbdc3432be647d7cb9
Kyle-Musngi <lkmusngi@gmail.com>
2025-10-10 16:23:13 +0800

    Second Commit

--- signapp/urls.py
+++ signapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('api/register/', views.register_user, name='register_user')
+    path('api/register/', views.register_user, name='register_user'),
+    path('api/users/', views.list_user, name='list_user'),
+    path('api/users/<int:pk>/', views.user_detail, name="user_detail")
 ]
