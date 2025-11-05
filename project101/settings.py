3942ae5bbf5c151e9a358b17137a2d51decda7d0
Kyle-Musngi <lkmusngi@gmail.com>
2025-11-05 10:29:37 +0800

    Add Render domain to ALLOWED_HOSTS

--- project101/settings.py
+++ project101/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8wi3u!c@2&+n*i=*pqa#8-s%*%xqsmm(i+t6_)(019c=u9(4&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://proj101.onrender.com"]
+ALLOWED_HOSTS = ['https://proj101.onrender.com', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://proj101.onrender.com",
